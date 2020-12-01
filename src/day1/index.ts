339fd5921a934dbea8f2d3dcbf60b2fc57e614c1
Josef Kuchař <josef.kuchar267@gmail.com>
2020-12-01 10:39:11 +0100

    Day 1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,20 +1,49 @@
 import { test, readInput } from "../utils/index"
 
-const prepareInput = (rawInput: string) => rawInput
+const prepareInput = (rawInput: string) =>
+  rawInput
+    .trim()
+    .split('\n')
+    .map(Number)
 
 const input = prepareInput(readInput('day1'))
 
-const goA = (input) => {
-  return
+const goA = (input: number[]) => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] + input[j] == 2020) {
+        return input[i] * input[j];
+      }
+    }
+  }
+  return null;
 }
 
 const goB = (input) => {
-  return
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      for (let k = j + 1; k < input.length; k++) {
+        if (input[i] + input[j] + input[k] == 2020) {
+          return input[i] * input[j] * input[k];
+        }
+      }
+    }
+  }
+  return null;
 }
 
 /*  Tests */
 
-// test()
+const testInput = prepareInput(`
+1721
+979
+366
+299
+675
+1456`);
+
+test(goA(testInput), 514579);
+test(goB(testInput), 241861950);
 
 /* Results */
 
